fffca6545e07d57002bbf35d6864a8b32c046a39
Anmol Sharma <anmolsharma2902@gmail.com>
2024-08-30 23:55:37 +0530

    30/8/2024 11:55pm

--- Week_2/2_promises.js
+++ Week_2/2_promises.js
@@ -1,10 +1,19 @@
+//promisfy this async function
+//no return
 function myOwnSetTimeout(fn, duration) {
-    setTimeout(fn, duration);
-  }
+  setTimeout(fn, duration);
+}
+
+//returning a promise
+function promisfiedMyOwnSetTimeout(duration){
+  return new Promise(function(resolve){
+    setTimeout(resolve, duration);
+  })
+}
   
+/* myOwnSetTimeout(function() {
+  console.log("log the first thing");
   myOwnSetTimeout(function() {
-    console.log("log the first thing");
-    myOwnSetTimeout(function() {
-      console.log("log the second thing");
-    }, 2000)
-  }, 1000)
+    console.log("log the second thing");
+  }, 2000)
+}, 1000) */
